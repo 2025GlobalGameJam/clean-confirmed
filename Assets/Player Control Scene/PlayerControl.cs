f2a9e49b69ae584cfb26c533f6a4f09d22666c4b
IanCuevas <iandcuevas28@gmail.com>
2025-01-25 14:13:47 -0700

    Camera Movement(Mostly Done)
    Just need to smoothen out movement with the camera, and then implementing rotation of character when rotating

--- Assets/Player Control Scene/PlayerControl.cs
+++ Assets/Player Control Scene/PlayerControl.cs
@@ -4,35 +4,26 @@ using UnityEngine.InputSystem;
 public class PlayerControl : MonoBehaviour
 {
     PlayerInput _playerInput;
-    InputAction _moveAction, _rotateAction;
+    InputAction _moveAction;
 
     public float moveSpeed;
-    public float rotateSpeed;
 
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         _playerInput = GetComponent<PlayerInput>();
         _moveAction = _playerInput.actions.FindAction("Move");
-        _rotateAction = _playerInput.actions.FindAction("Look");
     }
 
 
     void Update()
     {
         movePlayer();
-        rotatePlayer();
     }
 
     void movePlayer()
     {
         Vector2 direction = _moveAction.ReadValue<Vector2>();
         transform.position += new Vector3(direction.x, 0, direction.y) * moveSpeed * Time.deltaTime;
-    }
-
-    void rotatePlayer()
-    {
-        Vector2 rotateValue = _rotateAction.ReadValue<Vector2>();
-        rotateValue *= rotateSpeed * Time.deltaTime;
-        transform.Rotate(rotateValue.x, 0, rotateValue.y);
     }
 }
