6b713b83fcbd4cedb08120a66c128b351db508bb
Wonbin Lee <wblee800@gmail.com>
2025-01-26 01:31:35 -0700

    Add Game Timer for game time limitation

---
+++ Assets/GameTimer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float gameTime = 30f;
+    private bool isGameOver = false;
+
+    void Start()
+    {
+        StartCoroutine(StartTimer());
+    }
+
+    IEnumerator StartTimer()
+    {
+        yield return new WaitForSeconds(gameTime);
+        EndGame();
+    }
+
+    void EndGame()
+    {
+        isGameOver = true;
+        Debug.Log("Player failed to escape within the time limit!");
+
+        // Logic to stop players
+        // Stop all Rigidbodies
+        Rigidbody[] allRigidbodies = Object.FindObjectsByType<Rigidbody>(FindObjectsSortMode.None);
+        foreach (Rigidbody rb in allRigidbodies)
+        {
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            rb.isKinematic = true; // Disable physics operations
+        }
+
+        // Disable all moving scripts
+        PlayerController[] players = Object.FindObjectsByType<PlayerController>(FindObjectsSortMode.None);
+        foreach (PlayerController player in players)
+        {
+            player.enabled = false;
+        }
+
+        // Freezing the game (but not adjusting the timeScale so that the UI works)
+        Time.timeScale = 0f;
+    }
+}
